0e4518ec152b2b669e6c2256724b12f2c8f1ed10
amarnathdp <118151760+amarnathdp@users.noreply.github.com>
2023-11-28 19:58:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,18 +51,18 @@ var T = new Twit({
     consumer_secret: "0CQuw4o52Fi9ayejEYG9BcX1w3Pof8QGxnlwGGTnkopTisVLUd",
     access_token: "1729027293795033089-kSdl2L9QCoh78J3g2vASqUoXTNCREv",
     access_token_secret: "pfK6vgiVdoJsAfpyUv64iOiWodtma0j3LE8HFj6pAgRtm",
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL: false     // optional - requires SSL certificates to be valid.
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  // strictSSL: false     // optional - requires SSL certificates to be valid.
 })
 
-// let stream = T.stream('statuses/filter', { track: '#ODATwitterIntegration' })
+let stream = T.stream('statuses/filter', { track: ['#ODATwitterIntegration'] })
 // // 1729103096067096576Amarnatdp
 
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-    console.log(data)
-    // console.log(response);
-    console.log(err);
-  })
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//     console.log(data)
+//     // console.log(response);
+//     console.log(err);
+//   })
 
 //   var stream = T.stream('statuses/filter', { track: '#apple', language: 'en' })
  
@@ -70,30 +70,30 @@ T.post('statuses/update', { status: 'hello world!' }, function(err, data, respon
 //   console.log(tweet)
 // })
  
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-//   console.log(tweet.tweet)
-//   let user = tweet.user.screen_name
-//   let text = tweet.text
-//   let id = tweet.id_str
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+  console.log(tweet.tweet)
+  let user = tweet.user.screen_name
+  let text = tweet.text
+  let id = tweet.id_str
 
-//   const MessageModel = webhook.MessageModel();
-//   const message = {
-//     userId: user,
-//     messagePayload: MessageModel.textConversationMessage(text)
+  const MessageModel = webhook.MessageModel();
+  const message = {
+    userId: user,
+    messagePayload: MessageModel.textConversationMessage(text)
+  }
+
+  webhook.send(message)
+//   .then(()=>{
+//     console.log("Messahe Sent")
+//   }, e => console.log("error message" + e.message))
+
+//   try {
+//     webhook.send(message)
+//   } catch (error) {
+//     console.log(error);   
 //   }
-
-//   webhook.send(message)
-// //   .then(()=>{
-// //     console.log("Messahe Sent")
-// //   }, e => console.log("error message" + e.message))
-
-// //   try {
-// //     webhook.send(message)
-// //   } catch (error) {
-// //     console.log(error);   
-// //   }
-// })
+})
 
 // const client = new TwitterApi({
 //     appKey: "cp3dD2nxMzmt8WmzYD8LycG0H",
